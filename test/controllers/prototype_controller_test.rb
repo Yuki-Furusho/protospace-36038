a25576bac66ff3882c7aceab6330886554d7f17e
Yuki-Furusho <furusho@sikmi.com>
2021-08-13 13:49:37 +0900

    prototypes-controller、正解バージョン
    そもそも複数形で作成しなかったためにうまくいかなかったことが判明。

--- test/controllers/prototype_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PrototypeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get prototype_index_url
-    assert_response :success
-  end
-
-end
