1a2ac112c03f2fbbf418b0909efd90d94bd6fb93
Yuki-Furusho <furusho@sikmi.com>
2021-08-13 13:38:57 +0900

    prototype投稿機能の作成、途中経過
    そもそもcontroller名を単数にしていたことから、pathがおかしなことになっている可能性がある。いったんcommitして作り直して検証したい。

--- app/controllers/prototype_controller.rb
+++ app/controllers/prototype_controller.rb
@@ -1,4 +1,39 @@
 class PrototypeController < ApplicationController
   def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    if Prototype.create(proto_params)
+      redirect_to root_path(id: @prototype.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(proto_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def proto_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
